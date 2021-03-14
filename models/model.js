ff7b9d611e95146385a8412d24428cf8cbe73d70
mountain <Fiveoat@gmail.com>
2021-03-13 19:25:50 -0700

    x

--- models/model.js
+++ models/model.js
@@ -7,7 +7,7 @@ const pool = new Pool({
 });
 
 
-function getUserData(user_id, callback) {
+function getUserCoinData(user_id, callback) {
     let sql = "SELECT * FROM users u INNER JOIN user_coins uc on u.user_id = uc.user_id INNER JOIN coins c ON uc.coin_id = c.coin_id WHERE u.user_id=" + user_id;
     pool.query(sql, function (err, db_results) {
         if (err) {
@@ -24,22 +24,6 @@ function getUserData(user_id, callback) {
 function registerUser(first_name, last_name, password, email, callback) {
     let sql = "INSERT INTO users(first_name, last_name, password_, email) VALUES ('" + first_name + "', '" + last_name + "', '" + password + "', '" + email + "');";
     pool.query(sql, function (err, db_results) {
-        if (err) {
-            throw err;
-        } else {
-            let results = {
-                list: db_results.rows
-            };
-            callback(null, results);
-        }
-    })
-}
-
-
-function favoriteCoin(user_id, symbol, is_favorite, callback) {
-    let sql = "UPDATE user_coins SET is_favorite=$3::boolean WHERE user_id = $1::text AND symbol=$2::text"
-    var params = [user_id, symbol, is_favorite];
-    pool.query(sql, params, function (err, db_results) {
         if (err) {
             throw err;
         } else {
@@ -68,7 +52,6 @@ function addCoinHolding(user_id, symbol, quantity) {
 
 module.exports = {
     registerUser: registerUser,
-    getUserData: getUserData,
+    getUserCoinData: getUserCoinData,
     addCoinHolding: addCoinHolding,
-    favoriteCoin: favoriteCoin
 }
